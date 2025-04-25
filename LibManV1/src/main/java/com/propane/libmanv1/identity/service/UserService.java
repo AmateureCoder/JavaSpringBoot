043097af8219b0c6f2aefea848d212e1e2ded7d3
Charlie Brown <39505133+AmateureCoder@users.noreply.github.com>
2025-04-26 00:19:05 +0600

    Delete LibManV1 directory

--- LibManV1/src/main/java/com/propane/libmanv1/identity/service/UserService.java
+++
@@ -1,6 +0,0 @@
-package com.propane.libmanv1.identity.service;
-import com.propane.libmanv1.auth.dto.RegistrationDto;
-public interface UserService {
-    void register(RegistrationDto dto);
-    void updateRoles(Long userId, String roleName);
-}
