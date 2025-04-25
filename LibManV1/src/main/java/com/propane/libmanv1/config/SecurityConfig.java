ffd4156713eb6af06d9a0e7b158d17ae4aeb9cd7
Charlie Brown <39505133+AmateureCoder@users.noreply.github.com>
2025-04-25 21:20:24 +0600

    Update SecurityConfig.java

--- LibManV1/src/main/java/com/propane/libmanv1/config/SecurityConfig.java
+++ LibManV1/src/main/java/com/propane/libmanv1/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/register", "/login", "/css/**","/js/**","/assets/**").permitAll()
+                        .requestMatchers("/", "/home","/register", "/login", "/css/**","/js/**","/assets/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
