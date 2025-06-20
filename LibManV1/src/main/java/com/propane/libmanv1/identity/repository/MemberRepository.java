e03fa9d7925d78c69ab87b06287ec9f6aca60997
Charlie Brown <madeyemoodey62254@gmail.com>
2025-06-20 23:26:54 +0600

    Delete LibManV1 directory
    No longer viable

--- LibManV1/src/main/java/com/propane/libmanv1/identity/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.propane.libmanv1.identity.repository;
-
-import com.propane.libmanv1.identity.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
--- LibManV1/src/main/java/com/propane/libmanv1/identity/service/RoleService.java
+++
@@ -1,7 +0,0 @@
-package com.propane.libmanv1.identity.service;
-import com.propane.libmanv1.identity.model.Role;
-import java.util.Optional;
-public interface RoleService {
-    Optional<Role> findByName(String name);
-    Role ensureRole(String name);
-}
